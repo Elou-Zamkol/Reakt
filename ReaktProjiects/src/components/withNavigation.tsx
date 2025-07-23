416cd64407790453358abf10eb8bed980231a0a2
Elou-Zamkol <raulabdurahmanli2006@gmail.com>
2025-07-23 13:34:27 +0400

    Delete ReaktProjiects directory

--- ReaktProjiects/src/components/withNavigation.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import { useNavigate } from 'react-router-dom';
-
-const withNavigation = (Component: any) => (props: any) => {
-  const navigate = useNavigate();
-  return <Component {...props} navigate={navigate} />;
-};
-
-export default withNavigation;
